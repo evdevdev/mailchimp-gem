f07391c23bbb86f37b9cbe9405bdc827ac0300cd
Evan Arnold <evan.arnold@gmail.com>
2012-12-05 12:17:16 -0800

    initial commit from local files

---
+++ lib/mailchimp.rb
@@ -0,0 +1,15 @@
+require 'httparty'
+require 'json'
+require 'cgi'
+
+require "mailchimp/ext/httparty"
+
+require 'mailchimp/base'
+require "mailchimp/version"
+require "mailchimp/api"
+require "mailchimp/sts"
+require "mailchimp/mandrill"
+require 'mailchimp/export'
+
+module Mailchimp
+end
